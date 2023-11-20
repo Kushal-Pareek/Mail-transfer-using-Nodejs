662d6d17d4c7c3ee489421788b857d5ef6f069dd
Kushal-Pareek <88341691+Kushal-Pareek@users.noreply.github.com>
2023-11-20 14:42:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 var nodemailer = require('nodemailer');
+var XLSX = require('xlsx');
+
 var transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     auth: {
-        user: 'edmond.paucek@ethereal.email',
-        pass: 'upDmZgqFnMgBYGM9RG'
+        user: 'miguel51@ethereal.email',
+        pass: 'UyH9qf9YxpZ1nFWz7r'
     }
 });
-var recipients= ['garfield23@ethereal.email','anuragkhandelwal199@gmail.com'];
 
-for(var i=0;i<recipients.length;i++){
-var mailOptions = {
-    from: 'edmond.paucek@ethereal.email',
-    to: recipients[i],
-    subject: 'Sending Email using Node.js by Kushal Pareek',
-    html: '<h1>Hello Bro</h1><br></br><h2>What about MUMBAI trip</h2><p>How are you!</p>'
-};
 
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-});
+var workbook = XLSX.readFile('C:\\Users\\kusha\\OneDrive\\Desktop\\email_test\\dataset1.xlsx'); //dataset created in excel whose name is dataset1 and we are accessing it
+
+
+var sheet_name_list = workbook.SheetNames;
+var worksheet = workbook.Sheets[sheet_name_list[0]];
+
+
+var recipients = XLSX.utils.sheet_to_json(worksheet);
+console.log(recipients);
+
+
+for (var i = 0; i < recipients.length; i++) {
+    var recipientEmail = Object.values(recipients[i])[0]; 
+    var mailOptions = {
+        from: 'edmond.paucek@ethereal.email',
+        to: recipientEmail, // Use the recipient email
+        subject: 'Sending Email using Node.js by Kushal Pareek',
+        html: '<h1>Hello Bro</h1><br></br><h2>What about MUMBAI trip</h2><p>How are you!</p>'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 }
